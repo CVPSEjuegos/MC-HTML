96cedc4a9fe37345f55283fd1da9f6aef982ed0c
CVPSEjuegos <231303533@prepaenlinea-sep.edu.mx>
2026-03-01 23:56:22 -0600

    Refactor SaveSystem class for simplified saving/loading

--- MC_HTML_0.0.1ALPA/save_system.js
+++ MC_HTML_0.0.1ALPA/save_system.js
@@ -1,16 +1,13 @@
 export class SaveSystem {
     constructor() {
-        this.prefix = "MCHTML_SAVE_";
+        this.saveName = "MCHTML_WORLD_ALPHA";
     }
 
-    saveWorld(name, data) {
-        const blob = btoa(JSON.stringify(data)); // Simulación de formato .MCtx
-        localStorage.setItem(this.prefix + name, blob);
-        console.log(`Mundo ${name} guardado con éxito.`);
+    save(data) {
+        localStorage.setItem(this.saveName, JSON.stringify(data));
     }
 
-    loadWorld(name) {
-        const data = localStorage.getItem(this.prefix + name);
-        return data ? JSON.parse(atob(data)) : null;
+    load() {
+        return JSON.parse(localStorage.getItem(this.saveName));
     }
 }
