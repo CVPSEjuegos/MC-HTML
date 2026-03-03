cace2f3e2d87320583ad140f53e93e692c24bc41
CVPSEjuegos <231303533@prepaenlinea-sep.edu.mx>
2026-03-03 15:16:00 -0600

    Update world.js

--- MC_HTML_0.0.9ALPA/world.js
+++ MC_HTML_0.0.9ALPA/world.js
@@ -3,36 +3,32 @@ import * as THREE from 'three';
 export class World {
     constructor(scene) {
         this.scene = scene;
-        this.blocks = [];
+        
+        // NIEBLA: Color del cielo, distancia mínima, distancia máxima
+        // Esto oculta el final del mundo y ahorra recursos
+        scene.fog = new THREE.Fog(0x87ceeb, 5, 25); 
+
         this.generate();
     }
 
     generate() {
+        const size = 32; // Tamaño del mapa
         const geometry = new THREE.BoxGeometry(1, 1, 1);
         const material = new THREE.MeshStandardMaterial({ color: 0x55aa55 });
-
-        for (let x = 0; x < 16; x++) {
-            for (let z = 0; z < 16; z++) {
-                const mesh = new THREE.Mesh(geometry, material);
-                mesh.position.set(x, 0, z);
-                this.scene.add(mesh);
-                this.blocks.push(mesh);
+        
+        // Optimizamos: Usamos InstancedMesh en lugar de miles de Meshes individuales
+        this.mesh = new THREE.InstancedMesh(geometry, material, size * size);
+        
+        let i = 0;
+        const matrix = new THREE.Matrix4();
+        for (let x = 0; x < size; x++) {
+            for (let z = 0; z < size; z++) {
+                // Generación de terreno simple (puedes añadir Perlin Noise después)
+                matrix.setPosition(x - size/2, 0, z - size/2);
+                this.mesh.setMatrixAt(i, matrix);
+                i++;
             }
         }
-    }
-
-    breakBlock(camera) {
-        const raycaster = new THREE.Raycaster();
-        raycaster.setFromCamera({x: 0, y: 0}, camera);
-        const intersects = raycaster.intersectObjects(this.blocks);
-        if (intersects.length > 0) {
-            const obj = intersects[0].object;
-            this.scene.remove(obj);
-            this.blocks = this.blocks.filter(b => b !== obj);
-        }
-    }
-    
-    placeBlock(camera) {
-        // Lógica para poner bloque en la cara del seleccionado
+        this.scene.add(this.mesh);
     }
 }
