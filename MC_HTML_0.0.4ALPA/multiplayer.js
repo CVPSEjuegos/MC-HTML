bbed1b632c2ec014889713639b10891f7d379445
CVPSEjuegos <231303533@prepaenlinea-sep.edu.mx>
2026-03-03 13:57:25 -0600

    Update multiplayer.js

--- MC_HTML_0.0.4ALPA/multiplayer.js
+++ MC_HTML_0.0.4ALPA/multiplayer.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 
 export class Multiplayer {
-
     constructor(camera, scene) {
         this.camera = camera;
         this.scene = scene;
@@ -12,6 +11,11 @@ export class Multiplayer {
 
     host() {
         this.peer = new Peer();
+        this.peer.on('open', (id) => {
+            console.log("Tu ID de servidor es: " + id);
+            document.getElementById('val-peer').innerText = id;
+            alert("ID de tu servidor: " + id);
+        });
         this.peer.on('connection', conn => this.setupConnection(conn));
     }
 
@@ -20,40 +24,34 @@ export class Multiplayer {
         this.peer.on('open', () => {
             const conn = this.peer.connect(id);
             this.setupConnection(conn);
+            document.getElementById('val-peer').innerText = "Conectado a: " + id;
         });
     }
 
     setupConnection(conn) {
-
         this.connections.push(conn);
-
         conn.on('data', data => {
-
             if (!this.players[conn.peer]) {
                 const mesh = new THREE.Mesh(
-                    new THREE.BoxGeometry(0.8,1.8,0.8),
-                    new THREE.MeshStandardMaterial({color:0xff0000})
+                    new THREE.BoxGeometry(0.8, 1.8, 0.8),
+                    new THREE.MeshStandardMaterial({color: 0xff0000})
                 );
                 this.scene.add(mesh);
                 this.players[conn.peer] = mesh;
             }
-
             this.players[conn.peer].position.set(data.x, data.y - 1, data.z);
         });
-
     }
 
     update() {
-
+        if (!this.peer || this.connections.length === 0) return;
         const data = {
             x: this.camera.position.x,
             y: this.camera.position.y,
             z: this.camera.position.z
         };
-
         this.connections.forEach(conn => {
             if (conn.open) conn.send(data);
         });
-
     }
 }
