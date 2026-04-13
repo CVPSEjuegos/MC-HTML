b03f281fcb85cfec93ba50fd3ad8fc8c72c94318
CVPSEjuegos <231303533@prepaenlinea-sep.edu.mx>
2026-04-12 21:27:05 -0600

    Add Input module for keyboard event handling

---
+++ MC_HTML_0.1.5_ALPA/js/input.js
@@ -0,0 +1,14 @@
+const Input = {
+  keys: {},
+
+  init() {
+    window.addEventListener("keydown", e => this.keys[e.key] = true);
+    window.addEventListener("keyup", e => this.keys[e.key] = false);
+  },
+
+  isDown(key) {
+    return this.keys[key];
+  }
+};
+
+Input.init();
